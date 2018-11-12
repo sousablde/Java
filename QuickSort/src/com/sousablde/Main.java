c2880b649d8f78d14df99ce0fe24fe37528168f3
blacerda <blacerda@mail.ccsf.edu>
2018-11-12 15:47:16 -0800

    quick sort and notes

---
+++ QuickSort/src/com/sousablde/Main.java
@@ -0,0 +1,64 @@
+package com.sousablde;
+/*
+divide and conquer algorithm
+recursive
+uses pivot element to partition the array into 2 parts
+element < pivot is to the left and elements> pivot is to its right
+pivot will be in its correct sorted position
+it is an inplace algo just like with merge sort we end up partitioning the array into 1 element array series
+unstable algorithm
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        quickSort(intArray, 0, intArray.length);
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void quickSort(int[] input, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+
+        int pivotIndex = partition(input, start, end);
+        quickSort(input, start, pivotIndex);
+        for (int i = 0; i < input.length; i++) {
+            System.out.println(input[i]);
+        }
+        quickSort(input, pivotIndex + 1, end);
+    }
+
+    public static int partition(int[] input, int start, int end) {
+        // This is using the first element as the pivot
+        int pivot = input[start];
+        int i = start;
+        int j = end;
+
+        while (i < j) {
+
+            // NOTE: empty loop body
+            while (i < j && input[--j] >= pivot) ;
+            if (i < j) {
+                input[i] = input[j];
+            }
+
+            // NOTE: empty loop body
+            while (i < j && input[++i] <= pivot) ;
+            if (i < j) {
+                input[j] = input[i];
+            }
+
+        }
+
+        input[j] = pivot;
+        return j;
+
+    }
+}
+
